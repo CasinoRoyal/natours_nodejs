f8037ef56059510c46eaf4eb618354b3863370af
CasinoRoyal <lux_ex_tenebris@outlook.com>
2019-10-17 19:02:19 +0300

    Added slugify and include mongoose validation in tour schema

--- models/tour-model.js
+++ models/tour-model.js
@@ -1,26 +1,38 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
     unique: true,
-    trim: true
+    trim: true,
+    minlength: [5, 'Must be greather or equal then 5 characters'],
+    maxlength: [40, 'Must be less then 40 characters']
   },
+  slug: String,
   duration: {
     type: Number,
     required: [true, 'Duration is required']
   },
   maxGroupSize: {
     type: Number,
-    required: [true, 'maxGroup is required']
+    required: [true, 'maxGroup is required'],
+    min: [2, 'Must be greather then 2 pearsons'],
+    max: [60, 'Must be less then 40 persons']
   },
   difficulty: {
     type: String,
-    required: [true, 'difficulty is required']
+    required: [true, 'difficulty is required'],
+    enum: {
+      values: ['easy', 'medium', 'diffucult'],
+      message: 'It must be only: easy, medium, diffucult'
+    }
   },
   ratingsAverage: {
-    type: Number
+    type: Number,
+    min: [1, 'Must be greather then 1.0 pearsons'],
+    max: [5, 'Must be less then 5.0 persons']
   },
   ratingsQuantity: {
     type: Number
@@ -46,12 +58,26 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'Price is required']
   },
-  priceDiscount: Number,
+  priceDiscount: {
+    type: Number,
+    validate: {
+      validator: function(val) {
+        return val < this.price;
+      },
+      message: (props) => `${props.value} is greather then tour price`
+    }
+  },
   createdAt: {
     type: Date,
     default: Date.now()
   }
 });
+
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, {lower: true});
+  console.log(this)
+  next();
+})
 
 const Tour = mongoose.model('Tour', tourSchema);
 
