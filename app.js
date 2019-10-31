c5905848cff954df05efa9f693b35d1b31ac48bf
CasinoRoyal <lux_ex_tenebris@outlook.com>
2019-10-31 18:31:14 +0200

    Implemented sending token in cookie,  also security packages as helmet xss-clean hpp

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const tourRouter = require('./routes/tour-routers');
 const userRouter = require('./routes/user-routers');
@@ -8,11 +13,33 @@ const handleError = require('./controllers/error-controller');
 
 const app = express();
 
+app.use(helmet());
+
 if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 };
 
-app.use(express.json());
+const apiLimiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000
+});
+app.use('/api/', apiLimiter);
+
+app.use(express.json({ limit: '10kb' }));
+
+app.use(mongoSanitize());
+
+app.use(xss());
+
+app.use(hpp({
+  whitelist: [
+    'duration',
+    'price',
+    'difficulty',
+    'maxGroupSize',
+    'ratingsAverage'
+  ]
+}));
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -14,6 +14,16 @@ const generateToken = (id) => {
 
 const createAuthToken = (user, statusCode, res) => {
   const token = generateToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+  
+  if(process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
 
   return res.status(statusCode).json({
     status: 'success',
